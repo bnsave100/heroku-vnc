6ed3836d90bfd1164533e8dfff4c8c55bf4d3372
capriciousduck <capriciousduck@gmail.com>
2020-08-18 15:28:28 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,18 +6,6 @@ rm -rf /usr/share/icons/Adwaita
 cp -r /usr/share/icons/Humanity-Dark /usr/share/icons/Adwaita
 
 mkdir -p /Desktop
-cat << EOF >  /Desktop/Chrome.desktop
-[Desktop Entry]
-Version=1.0
-Type=Application
-Name=Google Chrome
-Comment=Access the Internet
-Exec=/usr/bin/google-chrome-stable --no-sandbox --disable-dev-shm-usage
-Icon=google-chrome
-Path=
-Terminal=false
-StartupNotify=true
-EOF
 
 cat << EOF >  /Desktop/AnyDesk.desktop
 [Desktop Entry]
@@ -32,18 +20,4 @@ Terminal=false
 StartupNotify=true
 EOF
 
-cat << EOF >  /Desktop/Swicth_to_Chinese_input.sh
-#!/bin/bash
-im-config -s ibus
-ibus-setup
-EOF
-
-cat << EOF >  /Desktop/Readme.txt
-Step 1: Excute Swicth_to_Chinese_input.sh on Deskop to switch input method.
-Step 2: Select 'Input Method', then click add button, choose Chinese and Pinyin icon to add.
-Step 3: Now it can support Chinese, Enjoy!
-EOF
-
-chmod +x /Desktop/Chrome.desktop
-chmod +x /Desktop/Swicth_to_Chinese_input.sh
 exec supervisord -c /app/supervisord.conf
